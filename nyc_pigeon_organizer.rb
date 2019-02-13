c958c07b439566ca579e863475db3ffc1df30e89
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-13 17:15:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attrib, attrib_hash|
+    attrib_hash.each do |stat, pigeon_array|
+      pigeon_array.each do |pigeon|
+        pigeon_list[pigeon] ||= {}
+        pigeon_list[pigeon][attrib] ||= []
+        pigeon_list[pigeon][attrib] << stat.to_s
+      end
+    end
+  end
+  pigeon_list
 end
