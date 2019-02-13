445b2b19b5e1d47981ef4d996737388af14c87ec
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-13 17:11:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,8 @@ def nyc_pigeon_organizer(data)
   data.each do |attrib, attrib_hash|
     attrib_hash do |stat, pigeon_array|
       pigeon_array do |pigeon|
-        pigeon_list ||= {}
         pigeon_list[pigeon] ||= {}
+        pigeon_list[pigeon][:attrib] ||= []
         pigeon_list[pigeon][:attrib] << stat
       end
     end
